2ec398eabe8cb61524b753ea759be18bed88e3a6
Pavel Kudishin <kudishin@mail.ru>
2019-09-22 11:48:21 +0400

    initial

---
+++ StatisticsCollection/StatisticsCollection/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,57 @@
+﻿using StatisticsCollection.Models;
+using StatisticsCollection.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StatisticsCollection.ViewModels
+{
+	public class ItemsViewModel : BaseViewModel
+	{
+		public ObservableCollection<Item> Items { get; set; }
+		public Command LoadItemsCommand { get; set; }
+
+		public ItemsViewModel()
+		{
+			Title = "Browse";
+			Items = new ObservableCollection<Item>();
+			LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+			MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+			{
+				Item newItem = item as Item;
+				Items.Add(newItem);
+				await DataStore.AddItemAsync(newItem);
+			});
+		}
+
+		async Task ExecuteLoadItemsCommand()
+		{
+			if (IsBusy)
+				return;
+
+			IsBusy = true;
+
+			try
+			{
+				Items.Clear();
+				IEnumerable<Item> items = await DataStore.GetItemsAsync(true);
+				foreach (Item item in items)
+				{
+					Items.Add(item);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+	}
+}
---
+++ StatisticsCollection/StatisticsCollection/Views/ItemsPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using StatisticsCollection.Models;
+using StatisticsCollection.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StatisticsCollection.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class ItemsPage : ContentPage
+	{
+		readonly ItemsViewModel _viewModel;
+
+		public ItemsPage()
+		{
+			InitializeComponent();
+
+			BindingContext = _viewModel = new ItemsViewModel();
+		}
+
+		async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+		{
+			Item item = args.SelectedItem as Item;
+			if (item == null)
+				return;
+
+			await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+			// Manually deselect item.
+			ItemsListView.SelectedItem = null;
+		}
+
+		async void AddItem_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			if (_viewModel.Items.Count == 0)
+				_viewModel.LoadItemsCommand.Execute(null);
+		}
+	}
+}
