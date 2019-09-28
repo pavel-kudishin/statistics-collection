8b6cb229558cd8793d6e5d89cdd7ab285a5096b9
Pavel Kudishin <kudishin@mail.ru>
2019-09-28 20:17:22 +0400

    handle exception on destroy

--- StatisticsCollection/StatisticsCollection.Android/MainActivity.cs
+++ StatisticsCollection/StatisticsCollection.Android/MainActivity.cs
@@ -1,14 +1,23 @@
-﻿
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Environment = System.Environment;
+using Platform = Xamarin.Essentials.Platform;
 
 namespace StatisticsCollection.Droid
 {
 	[Activity(Label = "StatisticsCollection", Icon = "@mipmap/icon",
-		Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+		Theme = "@style/MainTheme", MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -17,18 +26,103 @@ namespace StatisticsCollection.Droid
 
 			base.OnCreate(savedInstanceState);
 
-			global::Xamarin.Forms.Forms.SetFlags("Shell_Experimental", "Visual_Experimental",
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+			TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+
+
+			Forms.SetFlags("Shell_Experimental", "Visual_Experimental",
 				"CollectionView_Experimental", "FastRenderers_Experimental");
-			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			Platform.Init(this, savedInstanceState);
+			Forms.Init(this, savedInstanceState);
+
+			DisplayCrashReport();
+
 			LoadApplication(new App());
 		}
-		public override void OnRequestPermissionsResult(int requestCode, string[] permissions,
-			[GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+
+		protected override void OnDestroy()
 		{
-			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			try
+			{
+				base.OnDestroy();
+			}
+			catch (Exception e)
+			{
+			}
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions,
+			[GeneratedEnum] Permission[] grantResults)
+		{
+			Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
 			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+
+		private static void TaskSchedulerOnUnobservedTaskException(object sender,
+			UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+		{
+			Exception newExc = new Exception("TaskSchedulerOnUnobservedTaskException",
+				unobservedTaskExceptionEventArgs.Exception);
+			LogUnhandledException(newExc);
+		}
+
+		private static void CurrentDomainOnUnhandledException(object sender,
+			UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+		{
+			Exception newExc = new Exception("CurrentDomainOnUnhandledException",
+				unhandledExceptionEventArgs.ExceptionObject as Exception);
+			LogUnhandledException(newExc);
+		}
+
+		internal static void LogUnhandledException(Exception exception)
+		{
+			try
+			{
+				const string ERROR_FILE_NAME = "Fatal.log";
+				string libraryPath =
+					Environment.GetFolderPath(Environment.SpecialFolder
+						.Personal); // iOS: Environment.SpecialFolder.Resources
+				string errorFilePath = Path.Combine(libraryPath, ERROR_FILE_NAME);
+				string errorMessage = string.Format("Time: {0}\r\nError: Unhandled Exception\r\n{1}",
+					DateTime.Now, exception);
+				File.WriteAllText(errorFilePath, errorMessage);
+
+				// Log to Android Device Logging.
+				Log.Error("Crash Report", errorMessage);
+			}
+			catch
+			{
+				// just suppress any error logging exceptions
+			}
+		}
+
+		/// <summary>
+		// If there is an unhandled exception, the exception information is diplayed
+		// on screen the next time the app is started (only in debug configuration)
+		/// </summary>
+		[Conditional("DEBUG")]
+		private void DisplayCrashReport()
+		{
+			const string ERROR_FILENAME = "Fatal.log";
+			string libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string errorFilePath = Path.Combine(libraryPath, ERROR_FILENAME);
+
+			if (!File.Exists(errorFilePath))
+			{
+				return;
+			}
+
+			string errorText = File.ReadAllText(errorFilePath);
+			new AlertDialog.Builder(this)
+				.SetPositiveButton("Clear", (sender, args) => { File.Delete(errorFilePath); })
+				.SetNegativeButton("Close", (sender, args) =>
+				{
+					// User pressed Close.
+				})
+				.SetMessage(errorText)
+				.SetTitle("Crash Report")
+				.Show();
 		}
 	}
 }
